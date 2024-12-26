8a9fa2cf8aadb18087d24f7db90f8876fa9f3cef
Senan <senanaslan29@t-online.de>
2024-12-26 15:33:37 +0100

    added SplashScreen and ChatScreen, updated MainApp to switch screens based on authentication status

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:dart_chat/screens/chat.dart';
 import 'package:dart_chat/screens/log_in.dart';
 import 'package:dart_chat/screens/sign_up.dart';
+import 'package:dart_chat/screens/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import 'firebase_options.dart';
 
@@ -24,7 +27,16 @@ class MainApp extends StatelessWidget {
       theme: FlexThemeData.light(scheme: FlexScheme.cyanM3),
       darkTheme: FlexThemeData.dark(scheme: FlexScheme.cyanM3),
       themeMode: ThemeMode.system,
-      home: const LogInScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
+          return snapshot.hasData ? const ChatScreen() : const LogInScreen();
+        },
+      ),
     );
   }
 }
